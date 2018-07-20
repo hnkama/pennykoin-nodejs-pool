9261733a6c7ac54909f053056719942a7f88b30d
daHIFI <dahifi@gmail.com>
2018-07-20 17:42:31 -0400

    Added block.pennykoin.com block explorer

--- website_example/config.js
+++ website_example/config.js
@@ -6,9 +6,8 @@ var discord = "https://discord.gg/evK52Ej";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-// Empty string disables links on block and transaction links
-var blockchainExplorer = '' // replace when BE is live: "http://chainradar.com/{symbol}/block/{id}";
-var transactionExplorer = '' // replace when BE is live: "http://chainradar.com/{symbol}/transaction/{id}";
+var blockchainExplorer = "http://blocks.pennykoin.com/?hash={id}#blockchain_block"; 
+var transactionExplorer =  "http://blocks.pennykoin.com/?hash={id}#blockchain_transaction"; 
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
